b7ea3d0ff497da393dcab3eeb0095b3a5e1f5f92
Daniil Klementiev <klementevdaniil@gmail.com>
2024-02-25 10:21:32 +0200

    rating dialog and redirect to play market

--- app/src/main/java/com/example/hws/MainActivity.java
+++ app/src/main/java/com/example/hws/MainActivity.java
@@ -1,7 +1,12 @@
 package com.example.hws;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.widget.RatingBar;
+import android.widget.Toast;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,9 +15,67 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        showRatingDialog();
     }
 
-    public void showCustomNotification(View view) {
-        CustomNotification.showCustomNotification(this);
+    private void showRatingDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Rate Us What You Think");
+
+        RatingBar ratingBar = new RatingBar(this);
+        ratingBar.setNumStars(5);
+        ratingBar.setStepSize(1);
+
+        builder.setView(ratingBar);
+
+        builder.setPositiveButton("Submit", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                float rating = ratingBar.getRating();
+                if (rating >= 1 && rating <= 3) {
+                    showFeedbackDialog();
+                } else if (rating > 3 && rating <= 5) {
+                    goToPlayStore();
+                }
+            }
+        });
+
+        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                dialogInterface.dismiss();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void showFeedbackDialog() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setTitle("Feedback Dialog");
+        builder.setMessage("Please provide your feedback.");
+
+        builder.setPositiveButton("OK", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialogInterface, int i) {
+                Toast.makeText(MainActivity.this, "Thank you for your feedback!", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        builder.show();
+    }
+
+    private void goToPlayStore() {
+        // Замените "your.package.name" на реальный идентификатор вашего приложения в Google Play
+        String appPackageName = "your.package.name";
+        try {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("market://details?id=" + appPackageName)));
+        } catch (android.content.ActivityNotFoundException e) {
+            startActivity(new Intent(Intent.ACTION_VIEW,
+                    Uri.parse("https://play.google.com/store/apps/details?id=" + appPackageName)));
+        }
     }
 }
+
